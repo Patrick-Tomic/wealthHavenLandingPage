3ce037faf02b2f27a466c1a2acf63be09a4c5340
Patrick Tomic <patrick@Patricks-Air-3.lan>
2025-02-12 00:48:35 -0500

    start of landingpart of landing page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <header>
+      <img className='' src={logo} alt="" />
+    </header>
+    Hello world
+    </>
+  )
+}
+
+export default App
